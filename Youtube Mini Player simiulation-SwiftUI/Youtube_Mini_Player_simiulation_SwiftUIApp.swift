2ff14faec56f98ab682d990f0bc90b316586848a
Omar Adel <omar.adel888@hotmail.com>
2024-08-06 03:32:40 +0300

    Initial Commit

---
+++ Youtube Mini Player simiulation-SwiftUI/Youtube_Mini_Player_simiulation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_Mini_Player_simiulation_SwiftUIApp.swift
+//  Youtube Mini Player simiulation-SwiftUI
+//
+//  Created by Omar Adel on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_Mini_Player_simiulation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
